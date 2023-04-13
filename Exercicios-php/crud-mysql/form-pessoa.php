a91588521945d38ae7c6b07d7064376d05af8c1b
Kirmse <kirmse2005@gmail.com>
2023-04-12 22:36:52 -0300

    Site PHP

--- Exercicios-php/crud-mysql/form-pessoa.php
+++ Exercicios-php/crud-mysql/form-pessoa.php
@@ -7,7 +7,8 @@
 <body>
 <?php
 include 'conectar.php';
-$id = $nome = $email = $cpf = $sexo = "";
+include 'validar-cpf.php';
+$msgCpf = $id = $nome = $email = $cpf = $sexo = "";
 if($_SERVER["REQUEST_METHOD"] == "GET"){
     if (array_key_exists('id',$_GET)){
         $id = $_GET['id'];
@@ -23,21 +24,61 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
         echo $msg;
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $sexo = $_POST['sexo'];
+    $id = $_POST['id'];
+    
+    if(validarCpf($cpf)){
+        if($id == ''){
+            $msg = incluir($nome, $email, $cpf, $sexo);
+        } else {
+            $msg = alterar($id, $nome, $email, $cpf, $sexo);
+        }
+    }else{
+        $msgCpf = "CPF inválido!";
+    }
+    
+    echo $msg;
+}
+
 ?>
 <form action="form-pessoa.php" method="post">
     <input type="hidden" name="id"  value="<?php echo $id; ?>">
+    
     <h1>Formulário de Pessoa</h1>
-    Nome: <br>
-    <input type="text" name="nome" value="<?php echo $nome; ?>" required><br>
-    E-mail: <br>
-    <input type="text" name="email" value="<?php echo $email; ?>" required><br>
-    CPF: <br>
-    <input type="number" name="cpf" value="<?php echo $cpf; ?>" required><br>
-    Sexo: <br>
+    
+    <label>Nome: </label>
+    <input type="text" name="nome" placeholder="Nome completo" value="<?php echo $nome; ?>" required><br><br>
+    
+    <label>E-mail: </label>
+    <input type="text" name="email" placeholder="@gmail.com" value="<?php echo $email; ?>" required><br><br>
+    
+    <label>CPF: </label>
+    <input type="number" name="cpf" placeholder="000.000.000-00" value="<?php echo $cpf; ?>" required><br><br>
+    
+    <label>Escolaridade: </label>
+    <select name="Escolaridade" value="<?php echo $email; ?>"> 
+        <option value="">Selecione</option>
+        <option value="">Ensino Médio</option>
+        <option value="">Superior Incompleto</option>
+        <option value="">Superior Completo</option>
+    </select><br><br>
+    
+    <label>Sexo: </label>
     <input type="radio" name="sexo" value="m" required<?php if($sexo == "m") echo "checked"; ?>>Masculino
     <input type="radio" name="sexo" value="f" required<?php if($sexo == "f") echo "checked"; ?>>Feminino
-    <br>
-    <br>
+    <br><br>
+
+    <label>Senha: </label>
+    <input type="number" name="senha" placeholder="Senha" value="<?php echo ""; ?>" required><br><br>
+   
+    <label>Confirmar: </label>
+    <input type="number" name="senha" placeholder="Senha" value="<?php echo ""; ?>" required><br><br>
+
     <input type="submit" value="Gravar">
     <a href="form-pessoa.php">
     <input type="button" value="Novo">
@@ -48,24 +89,8 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     >
 </form>
 <?php
-//  onclick="window.location.replace('form-pessoa.php');"
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $cpf = $_POST['cpf'];
-    $sexo = $_POST['sexo'];
-
-    $id = $_POST['id'];
-    if($id == ''){
-       $msg = incluir($nome, $email, $cpf, $sexo);
-    } else {
-       $msg = alterar($id, $nome, $email, $cpf, $sexo);
-    }
-    
-    echo $msg;
-}
-
 ?>
+<br>
 <br>
 <br>
 <table border="1">
--- Exercicios-php/crud-mysql/validar-cpf.php
+++ Exercicios-php/crud-mysql/validar-cpf.php
@@ -1,34 +1,41 @@
-<?php 
-$cpf = "11144477735";
-validarCpf($cpf);
+<?php
+// $cpf = "26400633081";
+// validarCpf($cpf);
 
-function validarCpf(){
+function validarCpf($cpf){
     $cpf = str_split($cpf);
-    validarDigito($cpf, 0);
-    validarDigito($cpf, 1);
+    $d1 = validarDigito($cpf, 0);
+    $d2 = validarDigito($cpf, 1);
+    // if($cpf[9] == $d1 && $cpf[10] == $d2){
+    //     echo "CPF válido";
+    // }else{
+    //     echo "CPF inválido";
+    // }
+    return $cpf[9] == $d1 && $cpf[10] == $d2;
 }
+
 function validarDigito($cpf, $d){
-    $cpf = str_split($cpf);
+
     $soma = 0;
     for ($i=0; $i <= (8 + $d); $i++) { 
-        echo $i;
-        echo " - ";
-        echo (10 + $d)-$i;
-        echo " = ";
+        // echo $i;
+        // echo " - ";
+        // echo (10 + $d)-$i;
+        // echo " = ";
         $soma += $cpf[$i] * ((10 + $d)-$i);
-        echo $cpf[$i] * ((10 + $d)-$i);
-        echo "<br>";
+        // echo $cpf[$i] * ((10 + $d)-$i);
+        // echo "<br>";
     }
-    echo $soma;
+    // echo $soma;
     $digito = $soma % 11;
     if($digito >= 2){
         $digito = 11 - $digito;
     }else{
         $digito = 0;
     }
-    echo "<br> Digito = ";
-    echo $digito;
-    echo "<br>";
+    // echo "<br> digito = ";
+    // echo $digito;
+    // echo "<br>";
     return $digito;
 }
 ?>
