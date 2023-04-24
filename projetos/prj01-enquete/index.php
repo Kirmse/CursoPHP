42d39f629f2de00b915a32c671ff3f04e2c92531
Kirmse <kirmse2005@gmail.com>
2023-04-24 20:22:19 -0300

    Projeto01

---
+++ projetos/prj01-enquete/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <br><h1>Lista de Enquetes</h1>
+</body>
+</html>
+<br>
+<table class="table table-hover table-striped" id="tabela">
+    <th>Nome Enquete</th>
+    <th>Responder</th>
+    <th>Resultado</th>
+</table>
---
+++ projetos/prj01-enquete/script.sql
@@ -0,0 +1,24 @@
+create table enquete(
+    id int primary key auto_increment, 
+    nome varchar(200)
+);
+
+create table resposta(
+    id int primary key auto_increment,
+    id_enquete int not null,
+    nome varchar(200)
+    quantidade int,
+    foreign key (id_enquete) references enquete(id)
+);
+
+-- insert de uma nova enquere e respostas
+
+INSERT INTO enquete(nome) VALUES('Qual a sua linguagem de programação favorita?');
+
+-- para descobrir o id da esquete
+SELECT id FROM enquete WHERE nome = 'Qual a sua linguagem de programação favorita?';
+
+INSERT INTO resposta(id_enquete, nome, quantidade) VALUES(1,'Java',0);
+INSERT INTO resposta(id_enquete, nome, quantidade) VALUES(1,'PHP',0);
+INSERT INTO resposta(id_enquete, nome, quantidade) VALUES(1,'JavaScript',0);
+INSERT INTO resposta(id_enquete, nome, quantidade) VALUES(1,'C++',0);
