9bee3e84718d63a5ddd2c4ac5c8f4444d2e4f27d
Kirmse <kirmse2005@gmail.com>
2023-04-29 20:11:54 -0300

    Site Loja

---
+++ Exercicios-php/loja/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <style>
+        .div1 {
+            float: left;
+            margin: 10px;
+        }
+
+        .div2 {
+            float: right;
+        }
+
+        .div3 {
+            text-align: center;
+        }
+
+        .box {
+            float: right;
+            width: 25%;
+            padding: 5px;
+        }
+
+        .card {
+            display: inline-block;
+            width: 25%;
+        }
+    </style>
+
+    <div class="container mt-3">
+        <h1 class="div3">Doces da Dona Maria José</h1>
+
+        <div class="card" style="width:300px">
+            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">leite Ninho e Nutela</h4>
+                <div class="div1">R$: 45,00</div>
+                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+                </div>
+            </div>
+        </div>
+
+        <div class="card" style="width:300px">
+            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Brugadeiro e Maracujá</h4>
+                <div class="div1">R$: 45,00</div>
+                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+                </div>
+            </div>
+        </div>
+
+        <div class="card" style="width:300px">
+            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Brigadeiro e Limão</h4>
+                <div class="div1">R$: 45,00</div>
+                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+                </div>
+            </div>
+        </div>
+
+        <div class="card" style="width:300px">
+            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Cocolate e Oreo</h4>
+                <div class="div1">R$: 45,00</div>
+                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
