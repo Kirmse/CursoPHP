a9d4f87cc671775feab6c75298554bc772fd38e7
Kirmse <kirmse2005@gmail.com>
2023-04-29 17:47:37 -0300

    UpLoad Files

--- Exercicios-php/upload/index.php
+++ Exercicios-php/upload/index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Exemplo Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <form class="" action="upload.php" method="post" enctype="multipart/form-data">
-        Selecione a imagem: <br>
-        <input type="file" name="fileToUpload" ><br>
-        <input type="submit" value="Enviar Imagem" name="submit"><br>
-    </form>
+    <div class="container">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <br><h2>Selecione a imagem:</h2><br>
+            <input type="file" name="fileToUpload" class="btn btn-outline-secondary"><br><br>
+            <input type="submit" value="Enviar Imagem" class="btn btn-secondary" name="submit"><br>
+        </form>
+    </div>
 </body>
 </html>
