f397df1e78b8433eb7dfa6b1bba46a59c021aafa
Kirmse <kirmse2005@gmail.com>
2023-04-29 20:24:51 -0300

    Site Loja

--- Exercicios-php/loja/index.php
+++ Exercicios-php/loja/index.php
@@ -28,54 +28,64 @@
             padding: 5px;
         }
 
-        .card {
-            display: inline-block;
+        .card-style {
+            float: left;
             width: 25%;
+            padding: 50px;
         }
     </style>
 
     <div class="container mt-3">
         <h1 class="div3">Doces da Dona Maria José</h1>
 
-        <div class="card" style="width:300px">
-            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">leite Ninho e Nutela</h4>
-                <div class="div1">R$: 45,00</div>
-                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+        <div class="card-style">
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">leite Ninho e Nutela</h4>
+                    <div class="div1">R$: 45,00</div>
+                    <div class="div2"><a href="https://www.google.com/search?q=google+&client=opera-gx&hs=P5x&ei=rqZNZOfjKrCi1sQPnMmHgAI&ved=0ahUKEwjn8vzkndD-AhUwkZUCHZzkASAQ4dUDCA4&uact=5&oq=google+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQigUQsQMQQzILCAAQgAQQsQMQgwEyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMg4IABCABBCxAxCDARDJAzIICAAQigUQkgMyCAgAEIoFEJIDMgsIABCABBCxAxCDAToKCAAQRxDWBBCwAzoKCAAQigUQsAMQQ0oECEEYAFA-WD5g_wFoAXABeACAAYYBiAGGAZIBAzAuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp" class='btn btn-primary'>🛒</a></td>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="card" style="width:300px">
-            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">Brugadeiro e Maracujá</h4>
-                <div class="div1">R$: 45,00</div>
-                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+        <div class="card-style">
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">Brugadeiro e Maracujá</h4>
+                    <div class="div1">R$: 45,00</div>
+                    <div class="div2"><a href="https://www.google.com/search?q=google+&client=opera-gx&hs=P5x&ei=rqZNZOfjKrCi1sQPnMmHgAI&ved=0ahUKEwjn8vzkndD-AhUwkZUCHZzkASAQ4dUDCA4&uact=5&oq=google+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQigUQsQMQQzILCAAQgAQQsQMQgwEyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMg4IABCABBCxAxCDARDJAzIICAAQigUQkgMyCAgAEIoFEJIDMgsIABCABBCxAxCDAToKCAAQRxDWBBCwAzoKCAAQigUQsAMQQ0oECEEYAFA-WD5g_wFoAXABeACAAYYBiAGGAZIBAzAuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp" class='btn btn-primary'>🛒</a></td>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="card" style="width:300px">
-            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">Brigadeiro e Limão</h4>
-                <div class="div1">R$: 45,00</div>
-                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+        <div class="card-style">
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">Brigadeiro e Limão</h4>
+                    <div class="div1">R$: 45,00</div>
+                    <div class="div2"><a href="https://www.google.com/search?q=google+&client=opera-gx&hs=P5x&ei=rqZNZOfjKrCi1sQPnMmHgAI&ved=0ahUKEwjn8vzkndD-AhUwkZUCHZzkASAQ4dUDCA4&uact=5&oq=google+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQigUQsQMQQzILCAAQgAQQsQMQgwEyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMg4IABCABBCxAxCDARDJAzIICAAQigUQkgMyCAgAEIoFEJIDMgsIABCABBCxAxCDAToKCAAQRxDWBBCwAzoKCAAQigUQsAMQQ0oECEEYAFA-WD5g_wFoAXABeACAAYYBiAGGAZIBAzAuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp" class='btn btn-primary'>🛒</a></td>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="card" style="width:300px">
-            <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp" alt="Card image">
-            <div class="card-body">
-                <h4 class="card-title">Cocolate e Oreo</h4>
-                <div class="div1">R$: 45,00</div>
-                <div class="div2"><a href="finalizado.php" class='btn btn-primary'>🛒</a></td>
+        <div class="card-style">
+            <div class="card" style="width:300px">
+                <img class="card-img-top" src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">Cocolate e Oreo</h4>
+                    <div class="div1">R$: 45,00</div>
+                    <div class="div2"><a href="https://www.google.com/search?q=google+&client=opera-gx&hs=P5x&ei=rqZNZOfjKrCi1sQPnMmHgAI&ved=0ahUKEwjn8vzkndD-AhUwkZUCHZzkASAQ4dUDCA4&uact=5&oq=google+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQigUQsQMQQzILCAAQgAQQsQMQgwEyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMg4IABCABBCxAxCDARDJAzIICAAQigUQkgMyCAgAEIoFEJIDMgsIABCABBCxAxCDAToKCAAQRxDWBBCwAzoKCAAQigUQsAMQQ0oECEEYAFA-WD5g_wFoAXABeACAAYYBiAGGAZIBAzAuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp" class='btn btn-primary'>🛒</a></td>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 
 </body>
