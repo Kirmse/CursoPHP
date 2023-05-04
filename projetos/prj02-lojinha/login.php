33f8a6c642a710b7ac9dbffb4abcf55770c307a8
Kirmse <kirmse2005@gmail.com>
2023-05-04 20:57:43 -0300

    Projeto02

---
+++ projetos/prj02-lojinha/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <h2>Login</h2>
+                <form action="login.php" method="post">
+                    <div>
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" placeholder="@gmail.com" name="email">
+                    </div>
+                    <div>
+                        <label for="senha" class="form-label">Senha:</label>
+                        <input type="password" class="form-control" id="senha" placeholder="Crie uma senha" name="senha">
+                    </div><br>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <button type="submit" class="btn btn-secondary">Acessar Loja<a href="index.php"></a></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
