6cfda617a1a15f0a81c0bad058e34ca02da43c0d
Kirmse <kirmse2005@gmail.com>
2023-05-09 21:27:52 -0300

    Projeto02

---
+++ projetos/prj02-lojinha/listar-produto.php
@@ -0,0 +1,20 @@
+<div class="col-10">
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th class="col-1">Imagem</th>
+                <th class="col-1">Valor</th>
+                <th class="col-1">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>aaa</td>
+                <td>aaa</td>
+                <td>aaa</td>
+                <td>aaa</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
