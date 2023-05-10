8e93b17cf922a28c1ed8915897ebb7791bacfea9
Kirmse <kirmse2005@gmail.com>
2023-05-09 21:58:24 -0300

    Projeto02

--- projetos/prj02-lojinha/listar-produto.php
+++ projetos/prj02-lojinha/listar-produto.php
@@ -1,20 +1,32 @@
-<div class="col-10">
-    <table class="table table-hover table-striped">
-        <thead>
+<div class="row justify-content-center">
+    <div class="col-10">
+        <br>
+        <table class="table table-hover table-striped">
             <tr>
                 <th>Nome</th>
                 <th class="col-1">Imagem</th>
                 <th class="col-1">Valor</th>
-                <th class="col-1">Ações</th>
+                <th class="col-1" colspan="2">Ações</th>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>aaa</td>
-                <td>aaa</td>
-                <td>aaa</td>
-                <td>aaa</td>
-            </tr>
-        </tbody>
-    </table>
+
+            <?php 
+            $sql = "select * from produto";
+            $resultado = conectar($sql);
+            while ($linha = $resultado->fetch_assoc()) {
+                $nome = $linha["nome"];
+                $valor = $linha["valor"];
+                $imagem = $linha["imagem"];
+                $id = $linha["id"];
+                echo "
+                <tr>
+                    <td>$nome</td>
+                    <td><a href='$imagem' target='_blank' >🖼</a></td>
+                    <td>$valor</td>
+                    <td> <a href='admin.php?aditar=$id'>✏</a></td>
+                    <td><a href='admin.php?aditar=$id'>🗑</a></td>
+                </tr>";
+            }
+            ?>
+        </table>
+    </div>
 </div>
