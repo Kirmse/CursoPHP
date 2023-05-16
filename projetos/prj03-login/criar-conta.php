d7c6bedf7635fdb7801d3730ae559ac2bbcfcba8
Kirmse <kirmse2005@gmail.com>
2023-05-16 17:42:53 -0300

    Projeto 03

--- projetos/prj03-login/criar-conta.php
+++ projetos/prj03-login/criar-conta.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-
 <body>
+    <style>
+        a{
+            float: right;
+        }
+    </style>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-6">
-                <br><h2>Login 
-                    <h5><?php echo $acesso; ?></h5>
-                </h2><br>
-                <form action="login.php" method="post">
+                <br><h2>Cadastrar conta <a id="voltar" href="login.php" class="btn btn-outline-danger">Voltar</a></h2><br>
+                <form action="criar-conta.php" method="post">
                     <div>
                         <label for="email" class="form-label">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="@gmail.com" name="email">
-                    </div>
-                    <div><br>
+                        <input type="email" class="form-control" id="email" placeholder="Exemplo123@gmail.com" name="email">
+                    </div><br>
+                    <div>
                         <label for="senha" class="form-label">Senha:</label>
                         <input type="password" class="form-control" id="senha" placeholder="****" name="senha">
                     </div><br>
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                    <a href="criar-conta.php" class="btn btn-outline-primary">Criar Conta</a>
+                    <div>
+                        <label for="confirmar" class="form-label">Confimar senha:</label>
+                        <input type="password" class="form-control" id="confirmar" placeholder="****" name="confirmar">
+                    </div><br>
+        
+                    <button type="submit" class="btn btn-primary">Cadastrar</button>
                 </form>
             </div>
         </div>
     </div>
 </body>
-
 </html>
